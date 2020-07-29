fe3d490a60077da0dffd0754b1fb34874dcb04ac
Humberto Alonso <ict.alonso@gmail.com>
2020-07-29 18:29:11 +0200

    create module auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { AuthService } from './app/service/auth.service';
+import { AuthController } from './ui/auth.controller';
+import { UserService } from 'src/users/app/service/user.service';
+import { PassportModule } from '@nestjs/passport';
+import { UsersModule } from '../users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './app/strategies/jwt.strategy';
 
 @Module({
-  providers: [AuthService]
+  imports:[
+    PassportModule.register({defaultStrategy:'jwt', session:false}),
+    JwtModule.register({
+      secretOrPrivateKey:'estaesmiphrassecreate',
+      signOptions:{
+        expiresIn: 3600
+      }
+    }),
+    UsersModule],
+  providers: [AuthService,JwtStrategy],
+  controllers: [AuthController]
 })
 export class AuthModule {}
 
---
+++ src/auth/ui/auth.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Post, Body, HttpException, HttpStatus, Logger } from '@nestjs/common';
+import { LoginUserDto } from '../domain/dto/login-user-dto';
+import { AuthService } from '../app/service/auth.service';
+
+@Controller('auth')
+export class AuthController {
+    constructor(private authService: AuthService) { }
+
+    @Post()
+    async login(@Body() loginUserDto: LoginUserDto) {
+        let result = await this.authService.login(loginUserDto);
+        Logger.log("Resultado ->" + result);
+        return result;
+    }
+}
